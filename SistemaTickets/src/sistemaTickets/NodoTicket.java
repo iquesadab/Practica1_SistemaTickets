cb5b70457f2cfe8ab86df9ef6f64e95518801651
IVANNIA VANESSA QUESADA BOGANTES <iquesadab@ucenfotec.ac.cr>
2026-03-04 20:42:45 -0600

    [feat] Implementación de la estructura NodoTicket

--- SistemaTickets/src/sistemaTickets/NodoTicket.java
+++ SistemaTickets/src/sistemaTickets/NodoTicket.java
@@ -1,4 +1,27 @@
 package sistemaTickets;
 
 public class NodoTicket {
+
+    // Atributos
+    private Ticket ticket;
+    private NodoTicket siguiente;
+
+    // Constructor
+    public NodoTicket(Ticket ticket) {
+        this.ticket = ticket;
+        this.siguiente = null;
+    }
+
+    // Getters
+    public Ticket getTicket() {
+        return ticket;
+    }
+    public NodoTicket getSiguiente() {
+        return siguiente;
+    }
+
+    // Setter
+    public void setSiguiente(NodoTicket siguiente) {
+        this.siguiente = siguiente;
+    }
 }
