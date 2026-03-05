12e5bd77582ff85d9c7b292f02d7de024d9ba023
IVANNIA VANESSA QUESADA BOGANTES <iquesadab@ucenfotec.ac.cr>
2026-03-04 23:25:50 -0600

    [feat] Implementación de ListaEnlazadaResueltos

--- SistemaTickets/src/sistemaTickets/ListaEnlazadaResueltos.java
+++ SistemaTickets/src/sistemaTickets/ListaEnlazadaResueltos.java
@@ -1,4 +1,48 @@
 package sistemaTickets;
 
 public class ListaEnlazadaResueltos {
+
+    // Atributos
+    private NodoTicket primero;
+
+    // Constructor
+    public ListaEnlazadaResueltos() {
+        this.primero = null;
+    }
+
+    // Verificar si está vacía
+    public boolean estaVacia() {
+        return primero == null;
+    }
+
+    // Insertar al final
+    public void insertarAlFinal(Ticket ticket) {
+
+        NodoTicket nuevoNodo = new NodoTicket(ticket);
+
+        if (estaVacia()) {
+            primero = nuevoNodo;
+            return;
+        }
+        NodoTicket actual = primero;
+
+        while (actual.getSiguiente() != null) {
+            actual = actual.getSiguiente();
+        }
+        actual.setSiguiente(nuevoNodo);
+    }
+
+    // Buscar ticket por ID
+    public Ticket buscarPorId(int id) {
+
+        NodoTicket actual = primero;
+
+        while (actual != null) {
+            if (actual.getTicket().getId() == id) {
+                return actual.getTicket();
+            }
+            actual = actual.getSiguiente();
+        }
+        return null; // no encontrado
+    }
 }
