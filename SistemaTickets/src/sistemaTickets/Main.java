e6f6af135ab26d82c95a834b2bcce16885d73864
IVANNIA VANESSA QUESADA BOGANTES <iquesadab@ucenfotec.ac.cr>
2026-03-05 01:23:17 -0600

    [feat]: Se agrega la clase Main para ejecutar el sistema

--- SistemaTickets/src/sistemaTickets/Main.java
+++ SistemaTickets/src/sistemaTickets/Main.java
@@ -1,4 +1,10 @@
 package sistemaTickets;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        SistemaTickets sistema = new SistemaTickets();
+        sistema.iniciarSistema();
+    }
 }
